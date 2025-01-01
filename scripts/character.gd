9550e40dd4b09bd32fdb52d5f35dfc16e4a803ce
Jordon Bigelow <the-music-dude@hotmail.com>
2025-01-01 14:12:58 -0700

    updated locomation according to the tutorial instructions to make movement feel better

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,34 +1,46 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+
+@export var _speed: float = 8.0
+@export var _acceleration: float = 16.0
+@export var _deceleration: float = 32.0
 
 var _direction: float
 
 func face_left():
 	pass
 
+
 func face_right():
 	pass
+
 
 func run(direction: float):
 	_direction = direction
 
+
 func jump():
 	if is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+
+func _ready() -> void:
+	_speed *= Global.ppt
+	_acceleration *= Global.ppt
+	_deceleration *= Global.ppt
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if _direction:
-		velocity.x = _direction * SPEED
+	if _direction == 0:
+		velocity.x = move_toward(velocity.x, 0, _deceleration * delta)
+	elif velocity.x == 0 || sign(_direction) == sign(velocity.x):
+		velocity.x = move_toward(velocity.x, _direction * _speed, _acceleration * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, _direction * _speed, _deceleration * delta)
 	# Handle jump.
 	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 	# 	velocity.y = JUMP_VELOCITY
